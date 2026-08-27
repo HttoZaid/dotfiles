a428f8d235368cfd296b376cc0d4285d0566c0b0
ItsxZaid <zaidzeenathe@gmail.com>
2026-08-27 20:54:36 +0530

    feat: flutter support.

--- nvim/lua/plugins/framework.lua
+++ nvim/lua/plugins/framework.lua
@@ -4,10 +4,24 @@ return {
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
+      "stevearc/dressing.nvim",
     },
     config = function()
-      require("flutter-tools").setup({})
+      require("flutter-tools").setup({
+        widget_guides = { enabled = true },
+        closing_tags = { enabled = true, prefix = "// ", highlight = "Comment" },
+        dev_log = { enabled = true, open_cmd = "tabedit" },
+        dev_tools = {
+          autostart = true,
+          auto_open_browser = true,
+        },
+        lsp = {
+          color = { enabled = true },
+        },
+        decorations = {
+          statusline = { app_version = true, device = true },
+        },
+      })
     end,
   },
 }
